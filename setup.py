97666da1e506f4074825b0c9a977f22e7cc3eb0e
Andrew Austin <aaustin@redhat.com>
2022-03-09 17:23:01 -0600

    Change connection to local for Aruba provider

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def main():
 
     setuptools.setup(
         name='network-runner',
-        version='0.1.9',
+        version='0.1.10',
         description='Abstracton and Python API for Ansible Networking',
         long_description=long_description,
         author='Ansible',
